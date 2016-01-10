1824fd3596193351c8027af8ac1716b4dd0b53a2
Rosie <rosie@Fusion-Rosie-Shanley-MBP.local>
2016-01-10 01:18:51 -0600

    search multiple terms

--- scripts/main.js
+++ scripts/main.js
@@ -36,27 +36,17 @@ var App = React.createClass({
       filterText: filterText
     });
   },
-  filterItems : function() {
-
-  },
-  //renderItem : function(key){
-    //return <Item key={key} index={key} details={this.state.items[key]} filterText={this.state.filterText}/>
-  //},
   render : function() {
     return (
       <div className="gm-web-portal">
         <div className="menu">
           <Header tagline="Online Database of Academic Materials and Scholarly Research Pertaining to Genetic Modification" />
-          {/*<Search items={ITEMS} keys={KEYS} searchKey={KEY} />*/}
           <SearchBar
             filterText={this.state.filterText}
             onUserInput={this.handleUserInput} />
           <ItemList
             items={this.state.items}
             filterText={this.state.filterText} />
-          {/*<ul className="list-of-items">
-             {Object.keys(this.state.items).map(this.renderItem)}
-          </ul>*/}
         </div>  
       </div>
     )
@@ -76,15 +66,25 @@ var ItemList = React.createClass({
     filter = filter.split(" ");
     console.log(filter);
 
-    var checkFilter = function(obj, key) {
+    var filtered = function(term) {
+      for(var i=0;i<filter.length;i++){ 
+        if(term.toLowerCase().indexOf(filter[i]) > -1){
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+
+    var filterItems = function(obj, key) {
       Object.keys(obj).map(function(k) {
         var term = obj[k];
         if(typeof term == 'object' ) {
-          checkFilter(term);
+          filterItems(term);
           return;
         } else if (typeof term == 'number') {
           return;
-        } else if (term.toLowerCase().indexOf(filter) === -1) {
+        } else if ( filtered(term) ) {
           return;
         } else {
           if(used_keys.indexOf(key) == -1 && key !== undefined) {
@@ -99,7 +99,7 @@ var ItemList = React.createClass({
     Object.keys(items).map(function(key) {
       var thisItem = items[key];
       var thisKey = key;
-      checkFilter(thisItem, thisKey);
+      filterItems(thisItem, thisKey);
       return;
     });
 
@@ -117,46 +117,15 @@ var ItemList = React.createClass({
 */
 var Item = React.createClass({
   onButtonClick : function() {
-    // expand button
-    // console.log("Going to add the fish: ", this.props.index);
-    // var key = this.props.index;
-    // this.props.addToOrder(key);
+
   },
   render : function() {
     var details = this.props.details;
-    // var applyFilter = function(obj, filter) {
-    //   Object.keys(obj).map(function(j) {
-    //     var thisObj = obj;
-    //     console.log(thisObj)
-    //     if(filter == '') {
-    //       return thisObj.fitsSearchCriteria = true;
-    //     }
-    //     var term = obj[j];
-    //     thisObj.fitsSearchCriteria = false;
-    //     if(typeof term == 'object') {
-    //       return applyFilter(term);
-    //     } else if(term.toString().indexOf(filter) === -1) {
-    //       console.log('doesnt contain filter text');
-    //       return;
-    //     } else {
-    //       console.log('TEXT FOUND');
-    //       thisObj.fitsSearchCriteria = true;
-    //       console.log(term);
-    //       console.log('true');
-    //       return thisObj;
-    //     }
-    //   });
-    // }
-    // applyFilter(details, filter);
-
-    var shouldDisplay = (details.fitsSearchCriteria === true ? true : false);
     var isImage = (details.type === 'image' ? true : false);
     var isOpen = false;
     var buttonText = (isOpen ? 'Close!' : 'Open!'); 
-    // {buttonText} in JSX
-    //<button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
     return (
-      <li className={"menu-item " + details.type + " " + details.fitsSearchCriteria}>
+      <li className={"menu-item " + details.type}>
         <img src={details.link} alt={details.title} />
         <h3 className="menu-item-name">
           {details.author}
@@ -222,41 +191,3 @@ var routes = (
 )
 
 ReactDOM.render(routes, document.querySelector('#main'));
-
-
-
-    // var search_terms = [];
-
-    // var addToSearch = function(value) {
-    //   if(typeof value == 'number') {
-    //     return;
-    //   } else if (search_terms.indexOf(value) > -1) {
-    //     console.log('value repeated: ' + value);
-    //   } else {
-    //     search_terms.push(value);
-    //   }
-    // };
-
-    // var checkObj = function(obj) {
-    //   Object.keys(obj).map(function(j) {
-    //     var term = obj[j];
-    //     if(typeof term == 'object') {
-    //       checkObj(term);
-    //     } else {
-    //       addToSearch(term);
-    //     }
-    //   });
-    // };
-
-    // let ITEMS = [];
-    // let KEYS = [];
-    // let KEY = 'title';
-
-    // base.fetch('Articles', {
-    //   context: this,
-    //   asArray: true,
-    //   then(data){
-    //     ITEMS = data;
-    //     console.log(ITEMS);
-    //   }
-    // });
