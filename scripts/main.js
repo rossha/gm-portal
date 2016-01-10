a229982176f7eb66334ead205c55dca30a720b97
Rosie <rosie@Fusion-Rosie-Shanley-MBP.local>
2016-01-10 03:09:09 -0600

    header topic section loads in

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var ReactRouter = require('react-router');
-import Search from 'react-search';
 var Router  = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var History = ReactRouter.History;
@@ -22,6 +21,7 @@ var App = React.createClass({
   getInitialState : function() {
     return {
       items : {},
+      topics : {},
       filterText: ''
     }
   },
@@ -29,6 +29,10 @@ var App = React.createClass({
     base.syncState('Articles', {
       context: this,
       state: 'items'
+    });
+    base.syncState('Featured/topics', {
+      context: this,
+      state: 'topics'
     });
   },
   handleUserInput : function(filterText) {
@@ -40,8 +44,10 @@ var App = React.createClass({
     return (
       <div className="gm-web-portal">
         <div className="menu">
-          <Header tagline="Online Database of Academic Materials and Scholarly Research Pertaining to Genetic Modification" />
-          <SearchBar
+          <Header tagline="Academic Materials and Scholarly Research Pertaining to Genetic Modification" />
+          <TopicHeader 
+            topics={this.state.topics} />
+          <SearchInput
             filterText={this.state.filterText}
             onUserInput={this.handleUserInput} />
           <ItemList
@@ -50,6 +56,90 @@ var App = React.createClass({
         </div>  
       </div>
     )
+  }
+});
+
+/*
+  Header
+  <Header/>
+*/
+
+var Header = React.createClass({
+  render : function() {
+    return (
+      <header className="top">
+        <h1>GM Web Portal</h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3> 
+      </header>
+    )
+  }
+})
+
+/*
+  Topic Header
+  <TopicHeader/>
+*/
+
+var TopicHeader = React.createClass({
+  render : function() {
+    var topics = this.props.topics;
+    var topicBoxes = [];
+
+    Object.keys(topics).map(function(key) {
+      var thisTopic = topics[key];
+      var thisKey = key;
+      topicBoxes.push(<TopicBox key={thisKey} index={thisKey} details={thisTopic} />);
+    });
+    return (
+      <div className="topic-header">
+        {topicBoxes}
+      </div>
+    )
+  }
+})
+
+/*
+  Topic Box
+  <TopicBox/>
+*/
+
+var TopicBox = React.createClass({
+  onButtonClick : function() {
+
+  },
+  render : function() {
+    var details = this.props.details;
+    return (
+      <div className={"topic-box " + details.type}>
+        <p>{details.topic}</p>
+      </div>
+    )
+  }
+})
+
+/*
+  Search Bar
+  <SearchBar />
+*/
+
+var SearchInput = React.createClass({
+  handleChange: function() {
+    this.props.onUserInput(
+      this.refs.filterTextInput.value
+    );
+  },
+  render: function() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.filterText}
+          ref="filterTextInput"
+          onChange={this.handleChange}
+        />
+      </form>
+    );
   }
 });
 
@@ -64,7 +154,6 @@ var ItemList = React.createClass({
     var items = this.props.items;
     var filter = this.props.filterText.toLowerCase();
     filter = filter.split(" ");
-    console.log(filter);
 
     var filtered = function(term) {
       for(var i=0;i<filter.length;i++){ 
@@ -79,8 +168,8 @@ var ItemList = React.createClass({
     var filterItems = function(obj, key) {
       Object.keys(obj).map(function(k) {
         var term = obj[k];
-        if(typeof term == 'object' ) {
-          filterItems(term);
+        if(typeof term == 'object') {
+          filterItems(term, key);
           return;
         } else if (typeof term == 'number') {
           return;
@@ -115,6 +204,7 @@ var ItemList = React.createClass({
   Item
   <Item />
 */
+
 var Item = React.createClass({
   onButtonClick : function() {
 
@@ -136,49 +226,6 @@ var Item = React.createClass({
     )
   }
 });
-
-
-/*
-  Search Bar
-  <SearchBar />
-*/
-
-var SearchBar = React.createClass({
-  handleChange: function() {
-    this.props.onUserInput(
-      this.refs.filterTextInput.value
-    );
-  },
-  render: function() {
-    return (
-      <form>
-        <input
-          type="text"
-          placeholder="Search..."
-          value={this.props.filterText}
-          ref="filterTextInput"
-          onChange={this.handleChange}
-        />
-      </form>
-    );
-  }
-});
-
-/*
-  Header
-  <Header/>
-*/
-
-var Header = React.createClass({
-  render : function() {
-    return (
-      <header className="top">
-        <h1>GM Web Portal</h1>
-        <h3 className="tagline"><span>{this.props.tagline}</span></h3> 
-      </header>
-    )
-  }
-})
 
 /*
   Routes
